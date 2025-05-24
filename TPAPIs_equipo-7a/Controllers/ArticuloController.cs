93ce2fc44451be85f8fa6103fdc9fa858de58f4b
Agustin <agustinrodriguez2503@gmail.com>
2025-05-23 23:44:45 -0300

    validaciones de agregar

--- TPAPIs_equipo-7a/Controllers/ArticuloController.cs
+++ TPAPIs_equipo-7a/Controllers/ArticuloController.cs
@@ -35,20 +35,52 @@ namespace TPAPIs_equipo_7a.Controllers
         // POST: api/Articulo
         // Se agrega un producto a la base de datos (sin imagen),
         // luego de creado el articulo se le agrega la imagen en el POST de imagen, ya que necesitamos el IDArticulo.
-        public void Post([FromBody]ArticuloDto art)
+        public HttpResponseMessage Post([FromBody]ArticuloDto art)
         {
             ArticuloNegocio negocio = new ArticuloNegocio();
+            MarcaNegocio marcaNegocio = new MarcaNegocio();
+            CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+
+            //Validación de marca.
+            Marca idMarca = marcaNegocio.listar().Find(x => x.Id == art.Marca);
+            if (idMarca == null)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "La marca ingresada no existe.");
+
+            //Validación de categoria.
+            Categoria idCategoria = categoriaNegocio.listar().Find(x => x.Id == art.Categoria);
+            if (idCategoria == null)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "La categoría ingresada no existe.");
+
+            //Validación de nombre.
+            if (art.Nombre == null)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Debe ingresar un nombre.");
+
+            //Validación de descripción.
+            if (art.Descripcion == null)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Debe ingresar una descripción.");
+
+            //Validación de código.
+            if (art.Codigo == null)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Debe ingresar un código.");
+
+            //Validación de precio.
+            if (art.Precio <= 0)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Debe ingresar un precio mayor a 0.");
+
+
             Articulo nuevo = new Articulo();
             
             nuevo.Nombre = art.Nombre;
             nuevo.Codigo = art.Codigo;
             nuevo.Descripcion = art.Descripcion;
+            nuevo.Marca = new Marca();
             nuevo.Marca.Id = art.Marca;
+            nuevo.Categoria = new Categoria();
             nuevo.Categoria.Id = art.Categoria;
             nuevo.Precio = art.Precio;
 
             negocio.agregar(nuevo);
-
+            return Request.CreateResponse(HttpStatusCode.OK, "Artículo agregado correctamente.");
         }
 
         // PUT: api/Articulo/5
